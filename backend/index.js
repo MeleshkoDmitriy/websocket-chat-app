b7335dbb56687dc49a686edbf6a349a7f446986d
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-10-11 17:38:48 +0300

    feat: basic backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+import { indexRouter } from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
 
+// Middleware
+app.use(cors({ origin: "*"}));
+
+// Routes
+app.use("/", indexRouter);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
 });
